eaafdf9d95225c1c0130f99bf8fb09a9dc83334d
Alexanderfilippovich <filippovichaalexander@zohomail.eu>
2023-10-01 20:37:36 +0300

    инициализация компонент

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import HeaderComponent from '../components/HeaderComponent.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <HeaderComponent />
   </main>
 </template>
